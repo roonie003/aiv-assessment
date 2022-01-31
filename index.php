cb202919678993a870f25a078cbe45cc07d7d7cc
Roonie <roonierue@gmail.com>
2022-01-31 14:05:53 +0200

    navbar

---
+++ index.php
@@ -0,0 +1,51 @@
+<php?<!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://bootswatch.com/5/litera/bootstrap.min.css">
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+        <title>Spirit of Indonesia</title>
+    </head>
+
+    <body>
+
+        <nav class=" navfirst" id="nav">
+            <div class="container">
+                <div class="logo"><a href="/index.html"></div>
+                <div class="call">
+                    <i class="fa fa-phone-alt"></i>
+                    <spam>+1 631 440 3050</spam>
+                </div>
+                <div class="email">
+                    <i class="far fa-envelope"></i>
+                </div>
+                <div class="instagram">
+                    <i class="fab fa-instagram"></i>
+                </div>
+                <div class="facebook">
+                    <i class="fab fa-facebook"></i>
+                </div>
+                <div class="pinterest">
+                    <i class="fab fa-pinterest"></i>
+                </div>
+
+            </div>
+        </nav>
+
+
+        <nav class=" navtwo" id="nav">
+            <div class="container">
+                <ul>
+                    <li>Journe</li>
+                </ul>
+            </div>
+        </nav>
+
+
+    </body>
+
+    </html>
